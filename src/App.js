1b2d964107c416cbac3d22a426b45138da5a95a2
prince475 <danielmwai475@gmail.com>
2023-05-31 01:19:08 +0300

    Adding useState hook to allow the addition of list items on the page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
+import { useState } from 'react';
 import './App.css';
+
 
 function App() {
 
-  const carArray = ["Mercedes", "LandRover", "RangeRover"]
+  //using state to also update the List items of cars when the button is pressed.
 
-  const carItems = carArray.map(car => <li key={car}>{car}</li> )
-
+  const [car, setCar] = useState(["Mercedes", "LandRover", "RangeRover"]);
 
   function addItem(){
-    const newCarItem = `Car ${carArray.length + 1}`
-    carArray.push(newCarItem)
-    console.log(carArray)
+    const newCarItem = `Car ${car.length + 1}`
+    setCar(prevState => [...prevState, newCarItem])
+    // carArray.push(newCarItem)
+    // console.log(carArray)
   }
+
+  // const carArray = ["Mercedes", "LandRover", "RangeRover"]
+
+  const carItems = car.map(cars => <li className="list--item" key={cars}>{cars}</li> )
+
+
+
   return (
     <div className="App">
-      <button onClick={addItem}>Add Item</button>
+      <button className="form--button" onClick={addItem}>
+      Add Item
+      </button>
       {carItems}
     </div>
   );
